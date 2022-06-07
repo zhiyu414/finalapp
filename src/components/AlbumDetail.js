bc5382141f7eed8865f9109613dd3251ec0c93e3
zhiyu <910414tiffany@gmail.com>
2022-06-07 23:46:26 +0800

    no

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,26 +1,39 @@
-
-// import React, {useEffect, useState } from "react";
+// import React , { useState,useEffect }from "react";
 // import { Box, HStack, VStack, AspectRatio, Text, Image, Pressable,Center ,ScrollView } from "native-base"
+// import { TouchableOpacity,Animated,View} from "react-native"
+// import {updateSelector } from "../redux/selectors";
 // import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 // import { useDispatch, useSelector } from "react-redux";
-// import { addToCart } from "../redux/features/cartSlice";
-// import {Animated,View}from "react-native";
+// import { addToCart,removeItem,updateItem } from "../redux/features/cartSlice";
 // const AlbumDetail = ({ album, navigation }) => {
-//   const dispatch = useDispatch();
- 
-  
+//     const dispatch = useDispatch();
+//     //const cart = useSelector((state) => state.cart);
+//     const likestate = useSelector(updateSelector);
 //   const AnimatedIcon=Animated.createAnimatedComponent(MaterialCommunityIcons);
 //   const [liked,setliked]=useState(false);
 //   const [visible,setVisible]=useState(false);
 //   const currentValue =new Animated.Value(1);
+
+//   // const allAnime = useSelector((state) => state.allAnime)
+//   // const anime = useMemo(() => {
+//   //   for (let i = 0; i < allAnime.length; i += 1) {
+//   //     if (allAnime[i].id === animeIndex.id) {
+//   //       return allAnime[i]
+//   //     }
+//   //   }
+//   //   return animeIndex
+//   // }, allAnime)
+  
 //   useEffect(()=>{
 //     if(liked==true){
 //       Animated.spring(currentValue,{
 //         toValue:2,
-//         friction:2
+//         friction:2,
+//         useNativeDriver: true
 //       }).start(()=>{
 //         Animated.spring(currentValue,{
-//           toValue:1
+//           toValue:1,
+//           useNativeDriver: true
 //         }).start(()=>{
 //           setVisible(false)
 //         })
@@ -31,17 +44,17 @@
 //   return (
 //     <Box 
 //     flex={1}
-//     _dark={{ bg: "#4F5B57" }}
+//     _dark={{ bg: "#574E45" }}
 //     _light={{ bg: "#FEFFEF" }}
             
-//   > 
+// > 
 // <ScrollView>
 //     <Box
 //       marginLeft={29} marginRight={29} marginTop={5} borderRadius={4} shadow={2} 
-//       _dark={{ borderColor: 'blueGray.500', borderWidth: 0.6 }}  
+       
 //     >
-     
-//       <HStack  p={1} _dark={{ bg: "blueGray.900" }}
+      
+//       <HStack  p={1} _dark={{ bg: "#35322E" }}
 //         _light={{ bg: "white" }}>
           
 //         <Pressable 
@@ -49,20 +62,20 @@
 //         >
           
 //           <HStack  >
-//               {visible &&
+//           {visible &&
 //                     <AnimatedIcon  
 //                     style={{
 //                     position:"absolute",
 //                     top:30,
                     
-//                     left:"70%",
+//                     left:"90%",
 //                     elevation:4,
 //                     zIndex:3,
 //                     transform:[
 //                       {scale:currentValue}
 //                     ]
 //               }}
-//               name="heart" size={50} color="red"/>      
+//               name="heart" size={50} color="#ECD563"/>      
 //               }
               
 //             <Image
@@ -77,7 +90,7 @@
 //               style={{fontSize:16,}}
 //             >{album.name}</Text> 
 //             <Box position="absolute" top={-3} right={-13}>
-//             <MaterialCommunityIcons name="chevron-right" color="#574E45" size={25} />
+//             {/* <MaterialCommunityIcons name="chevron-right" color="#574E45" size={25} /> */}
 //             </Box>
 //             </Box>
 
@@ -85,35 +98,42 @@
 //               style={{fontSize:12,color:"#A5A5A5"}}
 //             >{album.gender}</Text>  
 //             </VStack>
-//             <Pressable
+//             {/* <TouchableOpacity
 //              onPress={() => {
 //               dispatch(addToCart(album));
 //             }}
-//             >
-//               <Box w={8} h={8} borderRadius={20} bgColor="#F9E6A1" position="absolute" top={35} right={-138}>
-//                   <Box position="absolute" top={1} right={1}>
-                  
-//                     <MaterialCommunityIcons name={liked?"heart":"heart-outline"} 
+//             > */}
+              
+//               <Box w={50} h={50} borderRadius={60} bgColor="#F9E6A1" position="absolute" top={30} right={-130}>
+//                   <Box position="absolute" top={2} right={2}>
+//                   <MaterialCommunityIcons name={liked?"heart":"heart-outline"} 
 //                     color="#574E45"
-//                      size={25} 
+//                      size={35} 
+                     
 //                      style={{
                       
 //                      }}
 //                     onPress={() => {
-//                       setliked(!liked);
+//                       // dispatch(updateItem());
+//                     setliked(!liked);
+                     
 //                       if(liked==false){
 //                         setVisible(true);
+
+                       
 //                       }
+                     
+//                       likestate?dispatch(removeItem(album.id)):dispatch(addToCart(album));
                       
-                      
-//                       dispatch(addToCart(album));
+                     
 //                     }}
 //                     />
-                   
+
 //                   </Box>
+//                   {/* <Text>{likestate}</Text> */}
 //                 </Box>
-//             </Pressable>
-               
+//             {/* </TouchableOpacity> */}
+                
 //           </HStack >
           
 //         </Pressable>
@@ -126,6 +146,8 @@
 //   )};
 
 // export default AlbumDetail;
+
+
 import React , { useState,useEffect }from "react";
 import { Box, HStack, VStack, AspectRatio, Text, Image, Pressable,Center ,ScrollView } from "native-base"
 import { TouchableOpacity,Animated,View} from "react-native"
--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,217 +1,9 @@
-// import React from 'react';
-// import { Linking } from 'react-native';
-// import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button, HStack ,VStack} from "native-base";
-
-
-// const DetailScreen = ({ route}) => {
-//   const { name,
-//         birthday,
-//         gender,
-//         process,
-//         Vaccine,
-//         image,
-//         people_close,
-//         cat_close,
-//         dog_close,
-//         microchip,
-//         ytest,
-//         otest,
-//         bug_inside,
-//         bug_ousitde,
-//         special_notice,
-//         habit
-//       } = route.params;
-//   return (
-//      <>
-//       <Image
-//             source={{uri: image }}
-//             alt='dogImage'
-//             w="100%"
-//             h={406}
-//             position="absolute"
-//             top={0}
-            
-//           />
-//       <ScrollView >
-//         <Center>
-//        <Box
-//           w="100%"
-//           // h={1000}
-//           marginTop={260}
-//          paddingBottom={5}
-          
-//           _dark={{ bg: "#574E45" }}
-//           _light={{ bg: "#FEFFEF" }}
-//           borderRadius={20}
-//        >
-         
-//           <Box 
-//           w={352}
-//           h={116}
-//           _dark={{ bg: "#35322E" }}
-//           _light={{ bg: "#fff" }}
-//           borderRadius={20}
-//           // position="absolute"
-//           marginTop={27}
-//           alignSelf= 'center'
-//          >
-//            <Text bold fontSize={20} marginLeft={27} marginTop={15}>{name}</Text>
-//            <HStack  marginTop={2} alignSelf= 'flex-start' marginLeft={5}>
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2} />
-//              <Text fontSize={15}>{birthday}</Text>
-//            </HStack>
-//            <HStack  marginTop={1} alignSelf= 'flex-start' marginLeft={5}>
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2} />
-//              <Text fontSize={15}>{gender}</Text>
-//            </HStack>
-           
-//          </Box> 
-//          <Box w={294} h={82} marginBottom={0} marginLeft={50}>
-//          <HStack marginTop={19}  alignSelf= 'flex-start' >
-//             <Text w={126}>
-//                <Text bold fontSize={15} color="#A5A5A5" >是否親人  </Text>
-//                {people_close}
-//              </Text>
-//              <Text marginLeft={45} fontSize={15} w={134}>
-//                <Text bold fontSize={15} color="#A5A5A5">是否親貓  </Text>
-//                {cat_close}
-//              </Text>
-             
-//          </HStack>
-//          <HStack alignSelf= 'flex-start' marginTop={4} marginRight={0}  w={40} h={20}>
-//             <Text fontSize={15} >
-//                <Text bold  color="#A5A5A5" >是否已植入晶片  </Text>
-//                {microchip}
-//              </Text>
-//              <Text marginLeft={10} fontSize={15} >
-//                <Text bold color="#A5A5A5">是否親狗  </Text>
-//                {dog_close}
-//              </Text>
-//          </HStack>
-//          </Box>
-//          <Box 
-//           w={352}
-//           h={170}
-//           _dark={{ bg: "#35322E" }}
-//           _light={{ bg: "#fff" }}
-//           borderRadius={20}
-//           // position="absolute"
-//           marginTop={19}
-//           alignSelf= 'center'
-//          >
-//            <ScrollView>
-//            <HStack alignSelf= 'flex-start' marginTop={13} marginLeft={5} >
-//              <Box w={2} h={5} bgColor="#ECD563" marginRight={2} marginTop={1}/>
-//              <Text w={290} fontSize={15}  lineHeight={30}>
-//                <Text  >救援經過:  </Text>
-//                {process}
-//              </Text>
-             
-//            </HStack>
-//            </ScrollView>
-         
-//          </Box> 
-//          <VStack alignSelf= 'center'   w={310} h={98} marginTop={19} marginBottom={0}>
-//          <HStack >
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
-//              <Text fontSize={15} bold>
-//                <Text fontWeight="normal">幼犬(2-6個月)驗腸炎、犬瘟快篩是否通過：</Text>
-//                { ytest}
-//              </Text>
-//            </HStack>
-//            <HStack marginTop={2}>
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
-//              <Text fontSize={15} bold >
-//                <Text fontWeight="normal">成犬(6個月以上)心絲蟲、艾莉希、萊姆病、伊文氏艾莉希體是否通過：</Text>
-//                {otest}
-//              </Text>
-//            </HStack>
-
-//          </VStack>
-//          <Box 
-//           w={352}
-//           h={140}
-//           _dark={{ bg: "#35322E" }}
-//           _light={{ bg: "#fff" }}
-//           borderRadius={20}
-//           // position="absolute"
-//           marginTop={19}
-//           alignSelf= 'center'
-//          >
-//            <HStack marginLeft={5} marginTop={13} w={300}>
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
-//              <Text fontSize={15} bold>
-//                <Text fontWeight="normal">預防針施打狀況(下一次施打時間): </Text>
-//                {Vaccine}
-//             </Text>
-//            </HStack>
-
-           
-//             <HStack marginLeft={5} marginTop={2}>
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
-//              <Text fontSize={15} bold>
-//                <Text fontWeight="normal">上一次體內驅蟲時間： </Text>
-//                {bug_inside}
-//             </Text>
-//            </HStack>
-
-            
-//             <HStack marginLeft={5} marginTop={2}>
-//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
-//              <Text fontSize={15}bold >
-//                <Text fontWeight="normal">上一次體外驅蟲時間：</Text>
-//                {bug_ousitde}
-//             </Text>
-//            </HStack>
-
-            
-//          </Box> 
-//          <VStack alignSelf= 'center'  w={310} h={67} marginTop={19} marginBottom={20}>
-//          <HStack >
-//              <Box w={2} h={5} bgColor="#ECD563" marginRight={2}/>
-//              <Text fontSize={15} bold>
-//                <Text fontWeight="normal">有沒有特別習慣：</Text>
-//                {habit}
-//              </Text>
-//            </HStack>
-
-            
-//              <HStack marginTop={5}>
-//              <Box w={2} h={5} bgColor="#ECD563" marginRight={2} />
-//              <Text fontSize={15} bold>
-//                <Text fontWeight="normal">有沒有什麼需要特別照顧的地方：</Text>
-//                {special_notice}
-//              </Text>
-//            </HStack>
-
-             
-//          </VStack>
-//        </Box>
-//        </Center>
-//        </ScrollView>
-       
-//       </>
-     
-//   );
-// }
-
-// export default DetailScreen;
-
-
-import React , { useState,useEffect }from 'react';
-import { Linking,TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Linking } from 'react-native';
 import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button, HStack ,VStack} from "native-base";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart,removeItem } from "../redux/features/cartSlice";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {updateSelector } from "../redux/selectors";
+
+
 const DetailScreen = ({ route}) => {
-  const dispatch = useDispatch();
-  const likestate = useSelector(updateSelector);
-  const cart = useSelector((state) => state.cart);
-  const [liked,setliked]=useState(false);
-  ;
-  const [visible,setVisible]=useState(false);
   const { name,
         birthday,
         gender,
@@ -231,8 +23,6 @@ const DetailScreen = ({ route}) => {
       } = route.params;
   return (
      <>
-      
-        
       <Image
             source={{uri: image }}
             alt='dogImage'
@@ -242,45 +32,6 @@ const DetailScreen = ({ route}) => {
             top={0}
             
           />
-         <TouchableOpacity
-             onPress={() => {
-              // dispatch(addToCart((route.params)));
-              // if({likestate}==0){
-              //   setliked(!liked);
-                
-              // }
-            }}
-            >
-        <Box w={50} h={50} borderRadius={60} bgColor="#F9E6A1"  top={30} right={-130}
-        
-        >
-
-                  <Box position="absolute" top={2} right={2}>
-
-                  <MaterialCommunityIcons name={liked?"heart":"heart-outline"} 
-                    color="#574E45"
-                     size={35} 
-                     
-                     style={{
-                      
-                     }}
-                     onPress={() => {
-                      setliked(!liked);
-                      dispatch(addToCart((route.params)));
-                      if({likestate}==0){
-                        setliked(!liked);
-                        
-                      }
-                     
-                      //dispatch(addToCart((route.params)));
-                      
-                     
-                    }}
-                    />
-                  </Box>
-                  <Text>{setliked}</Text>
-        </Box>
-         </TouchableOpacity>
       <ScrollView >
         <Center>
        <Box
@@ -293,8 +44,7 @@ const DetailScreen = ({ route}) => {
           _light={{ bg: "#FEFFEF" }}
           borderRadius={20}
        >
-           
-          
+         
           <Box 
           w={352}
           h={116}
@@ -316,14 +66,14 @@ const DetailScreen = ({ route}) => {
            </HStack>
            
          </Box> 
-         <Box w={294} h={100} marginBottom={0} marginLeft={50}>
+         <Box w={294} h={82} marginBottom={0} marginLeft={50}>
          <HStack marginTop={19}  alignSelf= 'flex-start' >
             <Text w={126}>
                <Text bold fontSize={15} color="#A5A5A5" >是否親人  </Text>
                {people_close}
              </Text>
-             <Text marginLeft={45} fontSize={15} w={150}>
-               <Text bold fontSize={15} color="#A5A5A5">是否親貓 </Text>
+             <Text marginLeft={45} fontSize={15} w={134}>
+               <Text bold fontSize={15} color="#A5A5A5">是否親貓  </Text>
                {cat_close}
              </Text>
              
@@ -333,7 +83,7 @@ const DetailScreen = ({ route}) => {
                <Text bold  color="#A5A5A5" >是否已植入晶片  </Text>
                {microchip}
              </Text>
-             <Text marginLeft={6} fontSize={15} >
+             <Text marginLeft={10} fontSize={15} >
                <Text bold color="#A5A5A5">是否親狗  </Text>
                {dog_close}
              </Text>
@@ -352,7 +102,7 @@ const DetailScreen = ({ route}) => {
            <ScrollView>
            <HStack alignSelf= 'flex-start' marginTop={13} marginLeft={5} >
              <Box w={2} h={5} bgColor="#ECD563" marginRight={2} marginTop={1}/>
-             <Text w={305} fontSize={15}  lineHeight={30}>
+             <Text w={290} fontSize={15}  lineHeight={30}>
                <Text  >救援經過:  </Text>
                {process}
              </Text>
@@ -446,3 +196,253 @@ const DetailScreen = ({ route}) => {
 }
 
 export default DetailScreen;
+
+
+// import React , { useState,useEffect }from 'react';
+// import { Linking,TouchableOpacity } from 'react-native';
+// import { Center, ScrollView, Box, AspectRatio, Text, Heading, Image, Button, HStack ,VStack} from "native-base";
+// import { useDispatch, useSelector } from "react-redux";
+// import { addToCart,removeItem } from "../redux/features/cartSlice";
+// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// import {updateSelector } from "../redux/selectors";
+// const DetailScreen = ({ route}) => {
+//   const dispatch = useDispatch();
+//   const likestate = useSelector(updateSelector);
+//   const cart = useSelector((state) => state.cart);
+//   const [liked,setliked]=useState(false);
+//   ;
+//   const [visible,setVisible]=useState(false);
+//   const { name,
+//         birthday,
+//         gender,
+//         process,
+//         Vaccine,
+//         image,
+//         people_close,
+//         cat_close,
+//         dog_close,
+//         microchip,
+//         ytest,
+//         otest,
+//         bug_inside,
+//         bug_ousitde,
+//         special_notice,
+//         habit
+//       } = route.params;
+//   return (
+//      <>
+      
+        
+//       <Image
+//             source={{uri: image }}
+//             alt='dogImage'
+//             w="100%"
+//             h={406}
+//             position="absolute"
+//             top={0}
+            
+//           />
+//          <TouchableOpacity
+//              onPress={() => {
+//               // dispatch(addToCart((route.params)));
+//               // if({likestate}==0){
+//               //   setliked(!liked);
+                
+//               // }
+//             }}
+//             >
+//         <Box w={50} h={50} borderRadius={60} bgColor="#F9E6A1"  top={30} right={-130}
+        
+//         >
+
+//                   <Box position="absolute" top={2} right={2}>
+
+//                   <MaterialCommunityIcons name={liked?"heart":"heart-outline"} 
+//                     color="#574E45"
+//                      size={35} 
+                     
+//                      style={{
+                      
+//                      }}
+//                      onPress={() => {
+//                       setliked(!liked);
+//                       dispatch(addToCart((route.params)));
+//                       if({likestate}==0){
+//                         setliked(!liked);
+                        
+//                       }
+                     
+//                       //dispatch(addToCart((route.params)));
+                      
+                     
+//                     }}
+//                     />
+//                   </Box>
+//                   <Text>{setliked}</Text>
+//         </Box>
+//          </TouchableOpacity>
+//       <ScrollView >
+//         <Center>
+//        <Box
+//           w="100%"
+//           // h={1000}
+//           marginTop={260}
+//          paddingBottom={5}
+          
+//           _dark={{ bg: "#574E45" }}
+//           _light={{ bg: "#FEFFEF" }}
+//           borderRadius={20}
+//        >
+           
+          
+//           <Box 
+//           w={352}
+//           h={116}
+//           _dark={{ bg: "#35322E" }}
+//           _light={{ bg: "#fff" }}
+//           borderRadius={20}
+//           // position="absolute"
+//           marginTop={27}
+//           alignSelf= 'center'
+//          >
+//            <Text bold fontSize={20} marginLeft={27} marginTop={15}>{name}</Text>
+//            <HStack  marginTop={2} alignSelf= 'flex-start' marginLeft={5}>
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2} />
+//              <Text fontSize={15}>{birthday}</Text>
+//            </HStack>
+//            <HStack  marginTop={1} alignSelf= 'flex-start' marginLeft={5}>
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2} />
+//              <Text fontSize={15}>{gender}</Text>
+//            </HStack>
+           
+//          </Box> 
+//          <Box w={294} h={100} marginBottom={0} marginLeft={50}>
+//          <HStack marginTop={19}  alignSelf= 'flex-start' >
+//             <Text w={126}>
+//                <Text bold fontSize={15} color="#A5A5A5" >是否親人  </Text>
+//                {people_close}
+//              </Text>
+//              <Text marginLeft={45} fontSize={15} w={150}>
+//                <Text bold fontSize={15} color="#A5A5A5">是否親貓 </Text>
+//                {cat_close}
+//              </Text>
+             
+//          </HStack>
+//          <HStack alignSelf= 'flex-start' marginTop={4} marginRight={0}  w={40} h={20}>
+//             <Text fontSize={15} >
+//                <Text bold  color="#A5A5A5" >是否已植入晶片  </Text>
+//                {microchip}
+//              </Text>
+//              <Text marginLeft={6} fontSize={15} >
+//                <Text bold color="#A5A5A5">是否親狗  </Text>
+//                {dog_close}
+//              </Text>
+//          </HStack>
+//          </Box>
+//          <Box 
+//           w={352}
+//           h={170}
+//           _dark={{ bg: "#35322E" }}
+//           _light={{ bg: "#fff" }}
+//           borderRadius={20}
+//           // position="absolute"
+//           marginTop={19}
+//           alignSelf= 'center'
+//          >
+//            <ScrollView>
+//            <HStack alignSelf= 'flex-start' marginTop={13} marginLeft={5} >
+//              <Box w={2} h={5} bgColor="#ECD563" marginRight={2} marginTop={1}/>
+//              <Text w={305} fontSize={15}  lineHeight={30}>
+//                <Text  >救援經過:  </Text>
+//                {process}
+//              </Text>
+             
+//            </HStack>
+//            </ScrollView>
+         
+//          </Box> 
+//          <VStack alignSelf= 'center'   w={310} h={98} marginTop={19} marginBottom={0}>
+//          <HStack >
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
+//              <Text fontSize={15} bold>
+//                <Text fontWeight="normal">幼犬(2-6個月)驗腸炎、犬瘟快篩是否通過：</Text>
+//                { ytest}
+//              </Text>
+//            </HStack>
+//            <HStack marginTop={2}>
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
+//              <Text fontSize={15} bold >
+//                <Text fontWeight="normal">成犬(6個月以上)心絲蟲、艾莉希、萊姆病、伊文氏艾莉希體是否通過：</Text>
+//                {otest}
+//              </Text>
+//            </HStack>
+
+//          </VStack>
+//          <Box 
+//           w={352}
+//           h={140}
+//           _dark={{ bg: "#35322E" }}
+//           _light={{ bg: "#fff" }}
+//           borderRadius={20}
+//           // position="absolute"
+//           marginTop={19}
+//           alignSelf= 'center'
+//          >
+//            <HStack marginLeft={5} marginTop={13} w={300}>
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
+//              <Text fontSize={15} bold>
+//                <Text fontWeight="normal">預防針施打狀況(下一次施打時間): </Text>
+//                {Vaccine}
+//             </Text>
+//            </HStack>
+
+           
+//             <HStack marginLeft={5} marginTop={2}>
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
+//              <Text fontSize={15} bold>
+//                <Text fontWeight="normal">上一次體內驅蟲時間： </Text>
+//                {bug_inside}
+//             </Text>
+//            </HStack>
+
+            
+//             <HStack marginLeft={5} marginTop={2}>
+//              <Box w={2} h={5} bgColor="#ECD563"  marginRight={2}/>
+//              <Text fontSize={15}bold >
+//                <Text fontWeight="normal">上一次體外驅蟲時間：</Text>
+//                {bug_ousitde}
+//             </Text>
+//            </HStack>
+
+            
+//          </Box> 
+//          <VStack alignSelf= 'center'  w={310} h={67} marginTop={19} marginBottom={20}>
+//          <HStack >
+//              <Box w={2} h={5} bgColor="#ECD563" marginRight={2}/>
+//              <Text fontSize={15} bold>
+//                <Text fontWeight="normal">有沒有特別習慣：</Text>
+//                {habit}
+//              </Text>
+//            </HStack>
+
+            
+//              <HStack marginTop={5}>
+//              <Box w={2} h={5} bgColor="#ECD563" marginRight={2} />
+//              <Text fontSize={15} bold>
+//                <Text fontWeight="normal">有沒有什麼需要特別照顧的地方：</Text>
+//                {special_notice}
+//              </Text>
+//            </HStack>
+
+             
+//          </VStack>
+//        </Box>
+//        </Center>
+//        </ScrollView>
+       
+//       </>
+     
+//   );
+// }
+
+// export default DetailScreen;
